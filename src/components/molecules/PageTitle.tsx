ceda1ed3daded1ecbb172448dbd710a92512b9ef
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-10 01:46:13 +0900

    PageTitle作成

--- src/components/molecules/PageTitle.tsx
+++ src/components/molecules/PageTitle.tsx
@@ -1,18 +1,28 @@
 import { memo } from "react"
+import styled from "styled-components"
 
 import { EnText } from "../atoms/EnText"
 import { ColorTheme } from "../../style/ColorTheme"
 import { JaText } from "../atoms/JaText"
 
-export const PageTitle = () => {
+export const PageTitle = memo(() => {
   return (
     <>
-      <EnText fSize='72px' fWeight='500' fStyle='italic'  fColor={ColorTheme.palette.purple}>
-        Peyo Log!
-      </EnText>
-      <JaText fSize='22px' fWeight='400' fColor={ColorTheme.palette.gray}>
-        Isono Tamaki’s portfolio site
-      </JaText>
+      <PageTitleWrap>
+        <EnText fSize='72px' fWeight='500' fStyle='italic'  fColor={ColorTheme.palette.purple}>
+          Peyo Log!
+        </EnText>
+        <JaText fSize='18px' fWeight='400' fColor={ColorTheme.palette.gray}>
+          Isono Tamaki’s portfolio site
+        </JaText>
+      </PageTitleWrap>
     </>
   )
-}
+})
+
+const PageTitleWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 6px;
+`;
